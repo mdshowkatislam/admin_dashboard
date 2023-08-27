167977094d8c29a849657bcfd983f4ea6cc8fd0a
mdshowkatislam <mdshowkatislam8876643@yahoo.com>
2023-08-27 16:32:41 +0600

    initial page and component settup and routes fixing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,34 @@
 import React from "react";
+import { Routes, Route } from "react-router";
+import Dashboard from "./pages/Dashboard";
+import Users from "./pages/Users";
+import Messages from "./pages/Messages";
+import Analytices from "./pages/Analytices";
+import FileManager from "./pages/FileManager";
+import Order from "./pages/Order";
+import Saved from "./pages/Saved";
+import Settings from "./pages/Settings";
+import Sidebar from "./components/Sidebar";
+import "./App.css";
 
 const App = () => {
   return (
-    <div>
-      <h1 className="text-4xl bg-red-800">App</h1>
-    </div>
+    <>
+      <Sidebar>
+        <Routes>
+          <Route path="/" element={<Dashboard />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/users" element={<Users />} />
+          <Route path="/messages " element={<Messages />} />
+          <Route path="/analytices" element={<Analytices />} />
+          <Route path="/fileManager" element={<FileManager />} />
+          <Route path="/order" element={<Order />} />
+          <Route path="/saved" element={<Saved />} />
+          <Route path="/settings" element={<Settings />} />
+          <Route path="*" element={<>not Found</>} />
+        </Routes>
+      </Sidebar>
+    </>
   );
 };
 
---
+++ src/pages/Analytices.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Analytices = () => {
+  return (
+    <div>Analytices</div>
+  )
+}
+
+export default Analytices
