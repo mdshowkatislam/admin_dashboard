7f5d421ec74863eb7539677cbfc4bffe919c9e35
mdshowkatislam <mdshowkatislam8876643@yahoo.com>
2023-08-29 03:08:54 +0600

    some style changes

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,6 +1,6 @@
-import React from "react";
+import React, { useState } from "react";
 import "../App.css";
-import { motion } from "framer-motion";
+import { motion, AnimatePresence } from "framer-motion";
 import { FaHome, FaBars, FaUser } from "react-icons/fa";
 import { MdMessage } from "react-icons/md";
 import { BiAnalyse, BiSearch, BiCog } from "react-icons/bi";
@@ -14,6 +14,17 @@ import { BsCartCheck } from "react-icons/bs";
 import { NavLink } from "react-router-dom";
 
 const Sidebar = ({ children }) => {
+  const [isOpen, setIsOpen] = useState(false);
+  const togleShow = () => setIsOpen(!isOpen);
+  const togleMotion = {
+    hide: { width: 0, padding: 0, opacity: 0, transition: { duration: 0.2 } },
+    show: {
+      width: "150px",
+      padding: "2px 2px",
+      opacity: 1,
+      transition: { duration: 0.2 },
+    },
+  };
   // all the sidebar items object file
   const allroute = [
     { path: "/", name: "Home", icon: <FaHome /> },
@@ -31,32 +42,42 @@ const Sidebar = ({ children }) => {
     { path: "/settings", name: "Settings  ", icon: <BiCog /> },
   ];
   return (
-    <div className=" main-container">
+    <div className="flex main-container">
       <motion.div
-        animate={{ width: "200px" }}
+        animate={{ width: isOpen ? "200px" : "46px" }}
         className="h-screen text-white sidebar bg-gradient-to-br from-blue-900 to-black"
       >
-        <div className="flex justify-between h-12 gap-3 p-2 text-lg transition ease-linear duration-250 item-center hover:border-white tansition hover:text-lg hover:shadow-md hover:shadow-white hover:bg-slate-800">
-          <h1 className="">youcoderbd</h1>
-          <div className="mt-1 transition-all ease-linear hover:border-white hover:scale-125">
-            <FaBars />
+        <div className="flex justify-between h-12 gap-3 p-3 text-lg transition ease-linear duration-250 hover:animate-pulse item-center hover:border-white tansition hover:text-lg hover:shadow-md hover:shadow-white hover:bg-slate-800">
+          {isOpen && <h1 className="">youcoderbd</h1>}
+          {/* <h1 className="">youcoderbd</h1> */}
+          <div className="mt-1 transition-all ease-linear hover:animate-spin hover:border-white hover:scale-125">
+            <FaBars onClick={togleShow} />
           </div>
         </div>
         <div className="flex justify-between h-12 gap-3 p-2 text-lg transition ease-linear duration-250 item-center hover:border-white tansition hover:text-lg hover:shadow-md hover:shadow-white hover:bg-slate-800">
           <div className="items-center mt-2 text-lg transition-all ease-linear hover:scale-150 ">
             {<BiSearch />}
           </div>
-          <input
-            placeholder="Search...."
-            className="w-40 p-2 rounded-md bg-slate-50 focus:outline-none"
-          />
+          <AnimatePresence>
+            {isOpen && (
+              <motion.input
+                initial="hide"
+                animate="show"
+                exit="hide"
+                variants={togleMotion}
+                placeholder="Search...."
+                className="w-40 p-2 rounded-md bg-slate-50 focus:outline-none"
+              />
+            )}
+          </AnimatePresence>
         </div>
+
         <section className="allroute ">
           {allroute.map((item) => (
             <NavLink
               to={allroute.path}
               key={allroute.name}
-              className="flex h-12 gap-3 p-2 text-lg transition-all ease-linear duration-250 item-center hover:border-r-8 hover:bg-slate-800 hover:border-white tansition hover:shadow-md hover:shadow-white"
+              className="flex h-12 gap-3 p-2 text-lg transition-all ease-linear hover:animate-pulse duration-250 item-center hover:border-r-8 hover:bg-slate-800 hover:border-white tansition hover:shadow-md hover:shadow-white"
             >
               <div className="p-1 ase-linear ptransition hover:scale-125">
                 {item.icon}
@@ -67,10 +88,9 @@ const Sidebar = ({ children }) => {
         </section>
       </motion.div>
 
-      <main className="text-black bg-red-600">{children}</main>
+      <main className="text-black ">{children}</main>
     </div>
   );
 };
 
 export default Sidebar;
- 
