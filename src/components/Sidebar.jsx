89b421178e407183d61f3de08c9dccdee05f318c
mdshowkatislam <mdshowkatislam8876643@yahoo.com>
2023-08-29 12:49:45 +0600

    Sidebar Navigation link problem fixed

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -16,11 +16,20 @@ import { NavLink } from "react-router-dom";
 const Sidebar = ({ children }) => {
   const [isOpen, setIsOpen] = useState(false);
   const togleShow = () => setIsOpen(!isOpen);
-  const togleMotion = {
+  const inputMotion = {
     hide: { width: 0, padding: 0, opacity: 0, transition: { duration: 0.2 } },
     show: {
       width: "150px",
       padding: "2px 2px",
+      opacity: 1,
+      transition: { duration: 0.2 },
+    },
+  };
+  const togleMotion = {
+    hide: { width: 0, opacity: 0, transition: { duration: 0.2 } },
+    show: {
+      width: "auto",
+
       opacity: 1,
       transition: { duration: 0.2 },
     },
@@ -48,8 +57,20 @@ const Sidebar = ({ children }) => {
         className="h-screen text-white sidebar bg-gradient-to-br from-blue-900 to-black"
       >
         <div className="flex justify-between h-12 gap-3 p-3 text-lg transition ease-linear duration-250 hover:animate-pulse item-center hover:border-white tansition hover:text-lg hover:shadow-md hover:shadow-white hover:bg-slate-800">
-          {isOpen && <h1 className="">youcoderbd</h1>}
-          {/* <h1 className="">youcoderbd</h1> */}
+          {isOpen && (
+            <AnimatePresence>
+              <motion.h1
+                initial="hide"
+                animate="show"
+                exit="hide"
+                variants={togleMotion}
+                className=""
+              >
+                youcoderbd
+              </motion.h1>
+            </AnimatePresence>
+          )}
+
           <div className="mt-1 transition-all ease-linear hover:animate-spin hover:border-white hover:scale-125">
             <FaBars onClick={togleShow} />
           </div>
@@ -64,7 +85,7 @@ const Sidebar = ({ children }) => {
                 initial="hide"
                 animate="show"
                 exit="hide"
-                variants={togleMotion}
+                variants={inputMotion}
                 placeholder="Search...."
                 className="w-40 p-2 rounded-md bg-slate-50 focus:outline-none"
               />
@@ -75,14 +96,25 @@ const Sidebar = ({ children }) => {
         <section className="allroute ">
           {allroute.map((item) => (
             <NavLink
-              to={allroute.path}
-              key={allroute.name}
+              to={item.path}
+              key={item.name}
               className="flex h-12 gap-3 p-2 text-lg transition-all ease-linear hover:animate-pulse duration-250 item-center hover:border-r-8 hover:bg-slate-800 hover:border-white tansition hover:shadow-md hover:shadow-white"
             >
               <div className="p-1 ase-linear ptransition hover:scale-125">
                 {item.icon}
               </div>
-              <div className="">{item.name}</div>
+
+              <AnimatePresence>
+                <motion.div
+                  initial="hide"
+                  animate="show"
+                  exit="hide"
+                  variants={togleMotion}
+                  className=""
+                >
+                  {item.name}
+                </motion.div>
+              </AnimatePresence>
             </NavLink>
           ))}
         </section>
